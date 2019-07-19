b89e995528cae2115bfadded90ca8130e18a4700
Punkte <paartheepan94@gmail.com>
2019-07-19 14:27:08 +0200

    wording

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ class Home extends Component {
             <div className="home_container">
                <div className="home_container--first">
                     <div className="home_container_text">
-                        <h1 className="home_container_text-title">Clash les joueurs de foot avec les tacles de footcaviar <span>footcaviar</span></h1>
+                        <h1 className="home_container_text-title">Génère des punchlines sur des joueurs avec <span>footcaviar</span></h1>
                         <p className="home_container_text-subtitle">Demande à ton assistant vocal de te trouver une punchline sur un joueur et partage la autour de toi.</p>
                     </div>
                </div> 
